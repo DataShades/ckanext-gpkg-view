e9b6ab9a0bb14739743c8a78ee430f6ae9f7ceaa
mutantsan <kvaqich@gmail.com>
2025-05-12 17:03:15 +0300

    fix: fix leaflet images path

--- ckanext/gpkg_view/assets/js/gpkg-view.js
+++ ckanext/gpkg_view/assets/js/gpkg-view.js
@@ -29,6 +29,9 @@ ckan.module('gpkg-view', function (jQuery, _) {
             self.el.append($("<div></div>").attr("id", "map"));
             self.map = ckan.commonLeafletMap('map', this.options.map_config, { attributionControl: false });
 
+            // // hack to make leaflet use a particular location to look for images
+            L.Icon.Default.imagePath = this.options.site_url + 'js/vendor/leaflet/images/';
+
             jQuery.getJSON(preload_resource['url']).done(
               function(data){
                 self.showPreview(data);
